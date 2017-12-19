5b020d9a1b3da3fe7246f5d053c7eec0f942279c
游品尧 <497400448@qq.com>
2017-12-19 17:36:05 +0800

    入口

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import ImageUpload from './src/components/image-upload';
+import Loading from './src/components/loading';
+import SecondMenu from './src/components/second-menu';
+import Table from './src/components/table';
+
+export {
+  ImageUpload,
+  Loading,
+  SecondMenu,
+  Table,
+};
