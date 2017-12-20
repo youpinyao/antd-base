0dbb444355113d4fb65bfb48ffd5a764c212c9ef
游品尧 <497400448@qq.com>
2017-12-20 10:04:31 +0800

    1

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import ImageUpload from './src/components/image-upload';
 import Loading from './src/components/loading';
 import SecondMenu from './src/components/second-menu';
 import Table from './src/components/table';
+import RootLayout from './src/components/root-layout';
 
 export {
   ImageUpload,
   Loading,
   SecondMenu,
   Table,
+  RootLayout,
 };
