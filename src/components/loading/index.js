eca066e41540befd481f8876477464e56b05615d
游品尧 <497400448@qq.com>
2018-01-05 17:03:09 +0800

    loading 继承style

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -8,11 +8,13 @@ import styles from './index.less';
 export default class Loading extends React.Component {
   render() {
     const { full, show } = this.props;
+    const style = this.props.style || {};
 
     // const icon = <Icon type="loading" style={{ fontSize: 60 }} spin />;
 
     return (
       <div
+        style={style}
         className={classnames(styles.loading, full ? styles.full : '', show ? styles.show : '')}
       >
         {/* <Spin className={styles.spin} size="large" tip={this.props.children} /> */}
